6e0940c2f30541e3b0ae339036c2bf3d965aad3e
terimakich <botxd495@gmail.com>
2025-06-26 12:00:35 +0530

    Update __main__.py

--- PURVIMUSIC/__main__.py
+++ PURVIMUSIC/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await PURVI.decorators()
     LOGGER("PURVIMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗔𝗟𝗣𝗛𝗔☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗜𝗡𝗖𝗥𝗜𝗖𝗜𝗕𝗟𝗘☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()

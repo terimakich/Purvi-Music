77a848b8187a5779a291ec335d0fc0e5bd2c529b
terimakich <botxd495@gmail.com>
2025-06-26 11:32:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ API_KEY = getenv("API_KEY", '30DxNexGenBots667d40') # youtube song api ke
 # ----------------------------------------------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THE_INCRICIBLE")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ZOYU_SUPPORT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/INCRICIBLE_SUPPORT")
 # ------------------------------------------------------------------------------
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "9000"))

c2352685d2f9acb44ed20a5adc3e123af626081e
terimakich <botxd495@gmail.com>
2025-06-26 12:05:05 +0530

    Update userbot.py

--- PURVIMUSIC/core/userbot.py
+++ PURVIMUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.one.join_chat("TEAMINCRICIBLE")
+                await self.one.join_chat("THE_INCRICIBLE")
             except:
                 pass
             assistants.append(1)

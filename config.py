20932b4a46533a1ed2baec4b6313430727cc01bc
terimakich <botxd495@gmail.com>
2025-08-12 10:40:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,6 +40,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", "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") 
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
 API_KEY = getenv("API_KEY", '30DxNexGenBots6d066c')
 
 # ----------------------------------------------------------------
